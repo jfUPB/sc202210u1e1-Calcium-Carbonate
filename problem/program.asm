869e025db28891c1113d2c3eece753e8164e99dd
Camilo Correa Rojas <camilo.correar@upb.edu.co>
2022-02-10 21:32:01 -0500

    push clase

--- problem/program.asm
+++ problem/program.asm
@@ -2,41 +2,49 @@
 // 418856
 // camilo.correar@upb.edu.co
 (START)
-    @10 
-    D=A 
-    @1 //dir CPI
-    M=D//
-    @1
-    D=M
-    @2 //dir CP
-    M=D//
-    @24576
-    D=M
-    @25
-    D;JEQ//
-    @12//L2
-    D=M
-    @15
-    D=D-A
-    @4//l1
-    D;JGT//
-    @2
-    A=M
-    M=1//
-    @2
-    M=M+1//
-    @12
-    0;JMP//
-    @25 //L3
-    D=M
-    @2
-    D=D-A
-    @4
-    D;JGT//
-    @2
-    M=M+1//
-    @2
-    M=M+1
-    @25
-    0;JMP
-    
+    @16384
+D=A
+@2 //cpi
+M=D //
+@2  //inicia el loop principal
+D=M
+@3 //cp
+M=D // cp
+@24576 //input
+D=M
+@70
+D=D-A
+@27
+D;JNE//
+@3
+D=M
+@24575
+D=D-A
+@4
+D;JGT//
+@3
+A=M
+M=-1// cambiar A -1
+@3
+M=M+1//
+@14
+0;JMP//
+@24576
+D=M
+@67
+D=D-A
+@4
+D;JNE//
+@3
+D=M
+@24575
+D=D-A
+@4//PONER EN 4 SI NO DA
+D;JGT//
+@3
+A=M
+M=0//
+@3
+M=M+1//
+@33
+0;JMP
